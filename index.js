425c3544f3c50c467391c6eacb92b5f15d0e8956
Peter deHaan <peter@deseloper.com>
2016-08-31 14:13:28 -0700

    Add a rough Joi schema validator for the metadata response

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fetch = require('node-fetch');
+const Joi = require('joi');
+const promisify = require('es6-promisify');
 const sortjson = require('sort-json');
+const joiValidate = promisify(Joi.validate);
 
 const EMBEDLY_PROXY = 'https://embedly-proxy.services.mozilla.com';
 const METADATA_PROXY = 'https://page-metadata-service.stage.mozaws.net';
@@ -8,10 +11,27 @@ const EMBEDLY_URI = `${EMBEDLY_PROXY}/v2/extract`;
 const METADATA_URI = `${METADATA_PROXY}/v1/metadata`;
 const RECOMMENDATIONS_URI = `${EMBEDLY_PROXY}/v2/recommendations`;
 
+const metadataSchema = Joi.object().keys({
+  description: Joi.string().required(),
+  favicon_url: Joi.string().uri().required(),
+  images: Joi.array().min(1).required(),
+  original_url: Joi.string().uri().required(),
+  title: Joi.string().required(),
+  url: Joi.string().uri().required()
+});
+
+function validateMetadata(site, schema, options) {
+  schema = schema || metadataSchema;
+  options = options || {abortEarly: false, allowUnknown: false};
+  return joiValidate(site, schema, options);
+}
+
 module.exports = {
   getEmbedly,
   getMetadata,
   getRecommendations,
+  metadataSchema,
+  validateMetadata,
   proxies: {
     embedly: EMBEDLY_URI,
     metadata: METADATA_URI,
---
+++ joi-test.js
@@ -0,0 +1,29 @@
+const { getRecommendations, getMetadata, validateMetadata } = require('./index');
+
+getRecommendations()
+  // Only scan a subset of the recommendations.
+  // NOTE: Maximum 20 URLs can be passed to Metadata server in a single request.
+  // .then((urls) => urls.slice(0, 10))
+
+  // Get Metadata for each Pocket recommendation.
+  .then((urls) => getMetadata(urls))
+  .then((results) => {
+    return Object.keys(results).map((site) => {
+      site = results[site];
+      // Validate each site's metadata against the default schema and store any errors.
+      return validateMetadata(site)
+        // Catch and inject any validation errors here so they don't break the
+        // `Promise.all()` below.
+        .catch(({details}) => {
+          site.errors = details;
+          return site;
+        });
+    });
+  })
+  // Wait for all Promises to resolve...
+  .then((promises) => Promise.all(promises))
+  // Only return results which have schema errors, for easier debugging.
+  .then((results) => results.filter((site) => !!site.errors))
+  // Make it pretty, or log the error if the Promise chain was broken.
+  .then((results) => console.log(JSON.stringify(results, null, 2)))
+  .catch((err) => console.error(err));
